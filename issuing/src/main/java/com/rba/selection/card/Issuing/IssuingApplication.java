0f599002d4d8bd1044483ef676ead606f1728653
Ivor Gradiški Zrinski <ivor.gradiski.zrinski@deepit.hr>
2024-07-10 14:15:16 +0200

    initial project commit

---
+++ issuing/src/main/java/com/rba/selection/card/Issuing/IssuingApplication.java
@@ -0,0 +1,13 @@
+package com.rba.selection.card.Issuing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuingApplication.class, args);
+	}
+
+}
---
+++ issuing/src/test/java/com/rba/selection/card/Issuing/IssuingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rba.selection.card.Issuing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
